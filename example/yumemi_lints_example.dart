935f5890d2bb6eb8cd8b6cb0cc603e5076f10457
YumNumm <onory0917@gmail.com>
2023-05-29 12:59:58 +0900

    init

---
+++ example/yumemi_lints_example.dart
@@ -0,0 +1,6 @@
+import 'package:yumemi_lints/yumemi_lints.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/yumemi_lints.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/yumemi_lints_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
