f9851f678f292328cb15d65c6a27ca919a5796bc
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2023-09-04 19:29:22 +0900

    feat: Create LintRuleService class

---
+++ tools/update_lint_rules/lib/src/services/lint_rule_service.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'lint_rule_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$lintRuleServiceHash() => r'5f360697d9b61cf3cbf54e6c0d18c5a0b8cdb8c0';
+
+/// See also [lintRuleService].
+@ProviderFor(lintRuleService)
+final lintRuleServiceProvider = AutoDisposeProvider<LintRuleService>.internal(
+  lintRuleService,
+  name: r'lintRuleServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$lintRuleServiceHash,
+  dependencies: <ProviderOrFamily>[appClientProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    appClientProvider,
+    ...?appClientProvider.allTransitiveDependencies
+  },
+);
+
+typedef LintRuleServiceRef = AutoDisposeProviderRef<LintRuleService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
