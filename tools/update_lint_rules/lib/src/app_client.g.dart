dd2073bd859c576aa572b3c212eb4c3b37fdbd96
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2023-09-04 19:23:36 +0900

    feat: Create AppClient class

---
+++ tools/update_lint_rules/lib/src/app_client.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'app_client.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appClientHash() => r'a1db46689e574368676c6fc136fb8cc93f2d0ffe';
+
+/// See also [appClient].
+@ProviderFor(appClient)
+final appClientProvider = AutoDisposeProvider<AppClient>.internal(
+  appClient,
+  name: r'appClientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appClientHash,
+  dependencies: const <ProviderOrFamily>[],
+  allTransitiveDependencies: const <ProviderOrFamily>{},
+);
+
+typedef AppClientRef = AutoDisposeProviderRef<AppClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
