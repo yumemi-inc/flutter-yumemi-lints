7b13a5b39d259050cb7a0eb8b6bfe5a3bf6cb8e5
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2023-09-05 10:03:11 +0900

    feat: Implement the process to get Dart SDK releases

--- tools/update_lint_rules/test/update_lint_rules_test.dart
+++ tools/update_lint_rules/test/update_lint_rules_test.dart
@@ -2,15 +2,18 @@ import 'package:riverpod/riverpod.dart';
 import 'package:test/test.dart';
 import 'package:update_lint_rules/src/models/exit_status.dart';
 import 'package:update_lint_rules/src/services/lint_rule_service.dart';
+import 'package:update_lint_rules/src/services/sdk_service.dart';
 import 'package:update_lint_rules/update_lint_rules.dart';
 
 import 'fakes/services/fake_lint_rule_service.dart';
+import 'fakes/services/fake_sdk_service.dart';
 
 void main() {
   test('updateLintRules', () async {
     final container = ProviderContainer(
       overrides: [
-        lintRuleServiceProvider.overrideWithValue(FakeLintRuleService())
+        lintRuleServiceProvider.overrideWithValue(FakeLintRuleService()),
+        sdkServiceProvider.overrideWithValue(FakeSdkService()),
       ],
     );
     addTearDown(container.dispose);
