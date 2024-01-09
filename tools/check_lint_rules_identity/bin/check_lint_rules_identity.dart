5f2b5463bab32133e6d0a228afb1babfe4cd534b
Yamasaki-pan961 <a_yamasaki@yumemi.co.jp>
2024-01-09 18:24:19 +0900

    feat: Create the dart project for check lint rules identity

---
+++ tools/check_lint_rules_identity/bin/check_lint_rules_identity.dart
@@ -0,0 +1,5 @@
+import 'package:check_lint_rules_identity/check_lint_rules_identity.dart' as check_lint_rules_identity;
+
+void main(List<String> arguments) {
+  print('Hello world: ${check_lint_rules_identity.calculate()}!');
+}
