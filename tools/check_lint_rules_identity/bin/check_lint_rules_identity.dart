f170307fc134bc0f7ce3e22e107f3333c31bd5c4
Yamasaki-pan961 <a_yamasaki@yumemi.co.jp>
2024-01-10 14:53:39 +0900

    style: Fix format

--- tools/check_lint_rules_identity/bin/check_lint_rules_identity.dart
+++ tools/check_lint_rules_identity/bin/check_lint_rules_identity.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 
-import 'package:check_lint_rules_identity/check_lint_rules_identity.dart' as check_lint_rules_identity;
+import 'package:check_lint_rules_identity/check_lint_rules_identity.dart'
+    as check_lint_rules_identity;
 
 void main(List<String> arguments) async {
-    final status = await check_lint_rules_identity.run(arguments);
+  final status = await check_lint_rules_identity.run(arguments);
   return Future.wait([
     stdout.close(),
     stderr.close(),
