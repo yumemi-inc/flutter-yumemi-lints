368600237a3593aa9a50a081cf8abff5b8578d81
Yamasaki-pan961 <a_yamasaki@yumemi.co.jp>
2024-01-10 18:01:49 +0900

    test: Remove meaningless tests

--- tools/check_lint_rules_identity/test/check_lint_rules_identity_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:check_lint_rules_identity/check_lint_rules_identity.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
