a45ff786f65c73b8295e9921aa60e75246e4b96d
morikann <mrknt0088@gmail.com>
2024-02-14 11:22:55 +0900

    feat: Define parseStringToVersion

---
+++ tools/check_lint_rules_identity/lib/src/utils/version_utils.dart
@@ -0,0 +1,19 @@
+import 'package:pub_semver/pub_semver.dart';
+
+// Version.parse cannot be used if minor does not exist, so I created my own parse method.
+Version parseStringToVersion(String versionString) {
+  final version = versionString.split('.');
+  try {
+    final major = int.parse(version[0]);
+    final minor = int.parse(version[1]);
+    const patch = 0;
+
+    return Version(
+      major,
+      minor,
+      patch,
+    );
+  } on FormatException catch (e) {
+    throw FormatException('Could not parse "$versionString". ${e.message}');
+  }
+}
