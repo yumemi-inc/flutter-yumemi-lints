4fbba8068843249f9b26951a7e02cb2763b540cc
morikann <mrknt0088@gmail.com>
2024-02-14 15:31:04 +0900

    refactor: Change parseStringToVersion argument name from versionString to version

--- tools/check_lint_rules_identity/lib/src/utils/version_utils.dart
+++ tools/check_lint_rules_identity/lib/src/utils/version_utils.dart
@@ -3,10 +3,10 @@ import 'package:pub_semver/pub_semver.dart';
 final _versionRegExp = RegExp(r'^(\d+)\.(\d+)$');
 
 // Version.parse cannot be used if patch does not exist, so I created my own parse method.
-Version parseStringToVersion(String versionString) {
-  final match = _versionRegExp.firstMatch(versionString);
+Version parseStringToVersion(String version) {
+  final match = _versionRegExp.firstMatch(version);
   if (match == null) {
-    throw FormatException('Could not parse "$versionString".');
+    throw FormatException('Could not parse "$version".');
   }
 
   try {
@@ -20,6 +20,6 @@ Version parseStringToVersion(String versionString) {
       patch,
     );
   } on FormatException catch (e) {
-    throw FormatException('Could not parse "$versionString". ${e.message}');
+    throw FormatException('Could not parse "$version". ${e.message}');
   }
 }
