393b4c17f44855e10fb503c01cc5da299c795e7c
morikann <mrknt0088@gmail.com>
2024-02-14 15:26:39 +0900

    fix: Use RegExp to determine if a string is a version

--- tools/check_lint_rules_identity/lib/src/utils/version_utils.dart
+++ tools/check_lint_rules_identity/lib/src/utils/version_utils.dart
@@ -1,11 +1,17 @@
 import 'package:pub_semver/pub_semver.dart';
 
+final _versionRegExp = RegExp(r'^(\d+)\.(\d+)$');
+
 // Version.parse cannot be used if patch does not exist, so I created my own parse method.
 Version parseStringToVersion(String versionString) {
-  final version = versionString.split('.');
+  final match = _versionRegExp.firstMatch(versionString);
+  if (match == null) {
+    throw FormatException('Could not parse "$versionString".');
+  }
+
   try {
-    final major = int.parse(version[0]);
-    final minor = int.parse(version[1]);
+    final major = int.parse(match[1]!);
+    final minor = int.parse(match[2]!);
     const patch = 0;
 
     return Version(
