8fea14d47ce83820eec7ccc5261c93f66cc3d0c5
morikann <mrknt0088@gmail.com>
2024-02-29 13:57:45 +0900

    feat: Define UpdateCommandService class

---
+++ packages/yumemi_lints/lib/src/command_services/update_command_service.dart
@@ -0,0 +1,10 @@
+import 'package:yumemi_lints/src/models/exit_status.dart';
+
+final class UpdateCommandService {
+  const UpdateCommandService();
+
+  ExitStatus call() {
+    return ExitStatus.success;
+  }
+}
+
