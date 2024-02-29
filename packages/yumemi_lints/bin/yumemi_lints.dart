df7aab974d327cd50164e0e945b5211096aa48f5
morikann <mrknt0088@gmail.com>
2024-02-29 14:00:35 +0900

    feat: Use AppCommandRunner to process commands

--- packages/yumemi_lints/bin/yumemi_lints.dart
+++ packages/yumemi_lints/bin/yumemi_lints.dart
@@ -1,1 +1,21 @@
-void main(List<String> args) {}
+import 'dart:io';
+
+import 'package:yumemi_lints/src/app_command_runner.dart';
+import 'package:yumemi_lints/src/models/exit_status.dart';
+
+Future<void> main(List<String> args) async {
+  final commandRunner = AppCommandRunner();
+  final exitStatus = await commandRunner.run(args);
+  await exitStatus.flushThenExit();
+}
+
+extension _FlushThenExit on ExitStatus {
+  Future<void> flushThenExit() async => Future.wait(
+        [
+          stdout.close(),
+          stderr.close(),
+        ],
+      ).then(
+        (_) => exit(code),
+      );
+}
