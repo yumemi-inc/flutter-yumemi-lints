102fb2df5c2f72fc9a81ae26e67f7dfba8992901
morikann <mrknt0088@gmail.com>
2024-02-29 14:05:33 +0900

    feat: Define _getProjectType to determine if the project is dart or flutter

--- packages/yumemi_lints/lib/src/command_services/update_command_service.dart
+++ packages/yumemi_lints/lib/src/command_services/update_command_service.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+import 'package:pub_semver/pub_semver.dart';
 import 'package:yumemi_lints/src/models/exit_status.dart';
+import 'package:yumemi_lints/src/models/project_type.dart';
 
 final class UpdateCommandService {
   const UpdateCommandService();
@@ -6,5 +11,31 @@ final class UpdateCommandService {
   ExitStatus call() {
     return ExitStatus.success;
   }
+
+  ProjectType _getProjectType() {
+    final pubspecFile = File(
+      path.join(
+        Directory.current.path,
+        'pubspec.yaml',
+      ),
+    );
+
+    if (!pubspecFile.existsSync()) {
+      throw Exception(
+        'The pubspec.yaml file could not be found in the current directory. '
+        'Please ensure that you are in the correct project directory '
+        'or create a pubspec.yaml file if it does not exist.',
+      );
+    }
+
+    final lines = pubspecFile.readAsLinesSync();
+    for (final line in lines) {
+      if (line.trim() == 'sdk: flutter') {
+        return ProjectType.flutter;
+      }
+    }
+    return ProjectType.dart;
+  }
+
 }
 
