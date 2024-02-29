f9d0a255c59da34a6babe8e94b9001b5c5afa785
morikann <mrknt0088@gmail.com>
2024-02-29 16:18:26 +0900

    fix: typo

--- packages/yumemi_lints/lib/src/utils/cli_info.dart
+++ packages/yumemi_lints/lib/src/utils/cli_info.dart
@@ -2,5 +2,5 @@ final class CliInfo {
   const CliInfo._();
 
   static const name = 'yumemi_lints';
-  static const description = 'A cli tool of yuemmi_lints';
+  static const description = 'A cli tool of yumemi_lints';
 }
