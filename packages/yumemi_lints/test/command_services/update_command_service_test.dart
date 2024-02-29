04e2af1650af9f40a3015d3e8829e9bb7a840047
morikann <mrknt0088@gmail.com>
2024-02-29 16:47:48 +0900

    test: Add getFlutterVersion group and change test description

--- packages/yumemi_lints/test/command_services/update_command_service_test.dart
+++ packages/yumemi_lints/test/command_services/update_command_service_test.dart
@@ -32,19 +32,22 @@ void main() {
     });
   });
 
-  // 関数毎にグループ化して例外時もテストするといいかも
-  test('get flutter version', () {
-    // arrange
-    const input = '''
+  group('UpdateCommandService.getFlutterVersion', () {
+    test(
+        'Successfully retrieve flutter version when appropriate input is received',
+        () {
+      // arrange
+      const input = '''
 Flutter 3.16.4 • channel stable • https://github.com/flutter/flutter.git
 Framework • revision 2e9cb0aa71 (3 months ago) • 2023-12-11 14:35:13 -0700
 Engine • revision 54a7145303
 Tools • Dart 3.2.3 • DevTools 2.28.4''';
 
-    // act
-    final version = updateCommandService.getFlutterVersion(input);
+      // act
+      final version = updateCommandService.getFlutterVersion(input);
 
-    // assert
-    expect(version, Version(3, 16, 4));
+      // assert
+      expect(version, Version(3, 16, 4));
+    });
   });
 }
