268458359ff3f89972f628a097e5591377e6eb43
morikann <mrknt0088@gmail.com>
2024-03-07 10:45:26 +0900

    test: Modified test code to match refactor

--- packages/yumemi_lints/test/command_services/update_command_service_test.dart
+++ packages/yumemi_lints/test/command_services/update_command_service_test.dart
@@ -5,111 +5,6 @@ import 'package:yumemi_lints/src/command_services/update_command_service.dart';
 
 void main() {
   const updateCommandService = UpdateCommandService();
-  group('UpdateCommandService.getDartVersion', () {
-    group('Successfully extract dart version', () {
-      // arrange
-      const sdks = [
-        '^2.17.0',
-        '2.17.0',
-        '>=2.17.0 <4.0.0',
-        '<4.0.0 >=2.17.0',
-      ];
-
-      final files = [
-        MemoryFileSystem().file('file')
-          ..writeAsStringSync(
-            '''
-environment:
-  sdk: '${sdks[0]}'
-''',
-          ),
-        MemoryFileSystem().file('file')
-          ..writeAsStringSync(
-            '''
-environment:
-  sdk: '${sdks[1]}'
-''',
-          ),
-        MemoryFileSystem().file('file')
-          ..writeAsStringSync(
-            '''
-environment:
-  sdk: '${sdks[2]}'
-''',
-          ),
-        MemoryFileSystem().file('file')
-          ..writeAsStringSync(
-            '''
-environment:
-  sdk: '${sdks[3]}'
-''',
-          ),
-      ];
-
-      for (var i = 0; i < files.length; i++) {
-        // act
-        final version = updateCommandService.getDartVersion(files[i]);
-
-        test('when input sdk version is ${sdks[i]}', () {
-          // assert
-          expect(
-            version,
-            Version(2, 17, 0),
-          );
-        });
-      }
-    });
-
-    group('Failure to get dart version', () {
-      // arrange
-      const versionErrorMessageScenarios = [
-        {
-          'version': 'any',
-          'errorMessage': 'Dart version could not be found from pubspec.yaml.'
-        },
-        {
-          'version': '<4.0.0',
-          'errorMessage': 'Please specify the minimum version.'
-        },
-      ];
-
-      final files = [
-        MemoryFileSystem().file('file')
-          ..writeAsStringSync(
-            '''
-environment:
-  sdk: '${versionErrorMessageScenarios[0]['version']}'
-''',
-          ),
-        MemoryFileSystem().file('file')
-          ..writeAsStringSync(
-            '''
-environment:
-  sdk: '${versionErrorMessageScenarios[1]['version']}'
-''',
-          ),
-      ];
-
-      for (var i = 0; i < files.length; i++) {
-        test(
-            'when input sdk version is ${versionErrorMessageScenarios[i]['version']}',
-            () {
-          // act, assert
-          expect(
-            () => updateCommandService.getDartVersion(files[i]),
-            throwsA(
-              isA<FormatException>().having(
-                (e) => e.message,
-                'errorMessage',
-                versionErrorMessageScenarios[i]['errorMessage'],
-              ),
-            ),
-          );
-        });
-      }
-    });
-  });
-
   group('UpdateCommandService.getFlutterVersion', () {
     test(
         'Throws FormatException '
@@ -135,5 +30,88 @@ environment:
         ),
       );
     });
+  });
+
+  group('UpdateCommandService.getDartVersion', () {
+    test(
+        'Throws FormatException '
+        'when the dart version is not listed in pubspec.yaml', () {
+      // arrange
+      final file = MemoryFileSystem().file('file')
+        ..writeAsStringSync(
+          '''
+environment:
+  flutter: '3.16.7'
+''',
+        );
+
+      // act, assert
+      expect(
+        () => updateCommandService.getDartVersion(file),
+        throwsA(
+          isA<FormatException>().having(
+            (e) => e.message,
+            'errorMessage',
+            'Please list the Dart version to be used in pubspec.yaml.',
+          ),
+        ),
+      );
+    });
+  });
+
+  group('UpdateCommandService.extractVersion', () {
+    // arrange
+    const versions = [
+      '^2.17.0',
+      '2.17.0',
+      '>=2.17.0 <4.0.0',
+      '<4.0.0 >=2.17.0',
+    ];
+
+    for (var i = 0; i < versions.length; i++) {
+      // act
+      final version = updateCommandService.extractVersion(versions[i]);
+
+      test(
+          'Successfully extract version '
+          'when input version is ${versions[i]}', () {
+        // assert
+        expect(
+          version,
+          Version(2, 17, 0),
+        );
+      });
+    }
+
+    // arrange
+    const versionErrorScenarios = [
+      {
+        'version': 'any',
+        'errorMessage': 'Version could not be found from pubspec.yaml.',
+      },
+      {
+        'version': '<4.0.0',
+        'errorMessage': 'Please specify the minimum version.',
+      },
+    ];
+
+    for (var i = 0; i < versionErrorScenarios.length; i++) {
+      test(
+          'Failure to extract version '
+          'when input version is ${versionErrorScenarios[i]['version']}', () {
+        // act, assert
+        expect(
+          () => updateCommandService
+              .extractVersion(versionErrorScenarios[i]['version']!),
+          throwsA(
+            isA<FormatException>().having(
+              (e) => e.message,
+              'errorMessage',
+              versionErrorScenarios[i]['errorMessage'],
+            ),
+          ),
+        );
+      });
+    }
   });
 }
