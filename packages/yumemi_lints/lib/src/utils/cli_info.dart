b3bc9d9a2ba8469b60dd196ddf5b291a594a9caf
morikann <mrknt0088@gmail.com>
2024-03-07 18:23:47 +0900

    chore: Match CliInfo.description with documentation comments in lin/yumemi_lints.dart

--- packages/yumemi_lints/lib/src/utils/cli_info.dart
+++ packages/yumemi_lints/lib/src/utils/cli_info.dart
@@ -2,5 +2,5 @@ class CliInfo {
   const CliInfo._();
 
   static const name = 'yumemi_lints';
-  static const description = 'A cli tool of yumemi_lints';
+  static const description = 'A simple CLI to manage yumemi_lints.';
 }
--- packages/yumemi_lints/lib/yumemi_lints.dart
+++ packages/yumemi_lints/lib/yumemi_lints.dart
@@ -1,4 +1,4 @@
-/// A simple CLI to manage yumemi_lints per project.
+/// A simple CLI to manage yumemi_lints.
 
 export 'package:yumemi_lints/src/app_command_runner.dart';
 export 'package:yumemi_lints/src/models/exit_status.dart';
