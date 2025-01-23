1b8a04a826c025dd92e8f336a4c25308517f9509
masa-futa <masakunn1215@gmail.com>
2025-01-23 15:27:16 +0900

    add: ArgParserException added

--- packages/yumemi_lints/lib/src/models/exceptions.dart
+++ packages/yumemi_lints/lib/src/models/exceptions.dart
@@ -1,3 +1,13 @@
 class CompatibleVersionException implements Exception {
   const CompatibleVersionException();
 }
+
+class ArgParserException implements Exception {
+  ArgParserException(this.message, this.commandName);
+
+  final String message;
+  final String commandName;
+
+  @override
+  String toString() => message;
+}
