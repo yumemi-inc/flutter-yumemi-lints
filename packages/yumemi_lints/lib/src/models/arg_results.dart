f028c805febeee3a46d4b1dea082d6f40f919f1e
masa-futa <masakunn1215@gmail.com>
2025-01-24 10:44:01 +0900

    change: Removed unnecessary extension classes due to the need for simpler Command operations.

--- packages/yumemi_lints/lib/src/models/arg_results.dart
+++
@@ -1,8 +0,0 @@
-import 'package:yumemi_lints/src/models/command.dart';
-
-/// Result class that wraps [Command]
-class ArgResults<T> {
-  const ArgResults(this.command);
-
-  final Command<T>? command;
-}
--- packages/yumemi_lints/lib/src/models/exceptions.dart
+++ packages/yumemi_lints/lib/src/models/exceptions.dart
@@ -1,13 +1,3 @@
 class CompatibleVersionException implements Exception {
   const CompatibleVersionException();
 }
-
-class ArgParserException implements Exception {
-  ArgParserException(this.message, this.commandName);
-
-  final String message;
-  final String commandName;
-
-  @override
-  String toString() => message;
-}
