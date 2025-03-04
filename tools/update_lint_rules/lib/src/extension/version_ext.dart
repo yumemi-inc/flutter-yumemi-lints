7ee360febc649893e457b7a8c8b004b86fe5013b
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2025-03-05 08:57:01 +0900

    chore: Remove tools-related files

--- tools/update_lint_rules/lib/src/extension/version_ext.dart
+++
@@ -1,5 +0,0 @@
-import 'package:pub_semver/pub_semver.dart';
-
-extension VersionExt on Version {
-  String get excludePatchVersion => '$major.$minor';
-}
--- tools/update_lint_rules/lib/src/models/severity_level.dart
+++
@@ -1,9 +0,0 @@
-/// Severity level of a lint rule.
-/// 
-/// See https://dart.dev/tools/analysis#customizing-analysis-rules
-enum SeverityLevel {
-  ignore,
-  info,
-  warning,
-  error,
-}
